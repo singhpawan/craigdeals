3266fa7b108480b584669a1cdad1417d443a97f2
singhpawan <pawandeep.me@gmail.com>
2016-01-26 04:29:34 -0500

    no commit

--- app.py
+++ app.py
@@ -24,18 +24,19 @@ def about():
 
 @app.route('/data', methods=['GET'])
 def data_func():
-    conn = psycopg2.connect(
+    """conn = psycopg2.connect(
         user="smqdidhwgocwmg",
         password="qRi2N64egyMRyHAN9tiQ42Bd0y",
         database="ec2-54-225-195-249.compute-1.amazonaws.com:5432/dbjjk6gfc81mbh")
     if not conn:
-        print "connection object not good"
-    cur = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+        print "connection object not good"""
+    #cur = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
     cmd = "SELECT * FROM priced WHERE date in (SELECT * FROM (SELECT date FROM priced WHERE model in ('accord', 'civic', 'camry', 'corolla') ORDER BY date) as t) ORDER BY delta DESC;"
 
-    cur.execute(cmd)
-    #data = engine.execute(cmd)
-    data = cur.fetchall()
+    #cur.execute(cmd)
+    with engine.connect() as conn:
+        data = conn.execute(cmd)
+    #data = cur.fetchall()
     return jsonify(items=list(data))
 
 
